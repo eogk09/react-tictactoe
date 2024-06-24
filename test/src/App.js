7146eedaa1df96029eb26ec1948e99baff22266b
eogk09 <eogk09@naver.com>
2024-06-24 15:19:43 +0900

    틱택토

---
+++ test/src/App.js
@@ -0,0 +1,20 @@
+
+import './App.css';
+import Board from './components/Board';
+
+function App() {
+  return (
+    
+    <div className="game">
+      <div className="game-borad">
+      <Board/>
+      </div>
+      <div className="game-info">
+      game-info
+      </div>
+    </div>
+   
+  );
+}
+
+export default App;

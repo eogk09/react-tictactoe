89fb4334f5b87d4e8d275c0c94c658b8528077b8
eogk09 <eogk09@naver.com>
2024-06-26 17:07:15 +0900

    20240626

--- test/src/components/Square.js
+++ test/src/components/Square.js
@@ -2,12 +2,18 @@ import React from "react";
 import './Square.css';
 
 export default class Square extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            value: null,
+        };
+    }
     render(){
         return (
             <button className="square" onClick={()=>{
-                console.log('click');
+                this.setState({value: 'X'});
             }}>
-                {this.props.value}
+                {this.state .value}
             </button>
         )
     }
